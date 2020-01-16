df1952b25b7a079704fa388f07c8af571b1bc99e
Tom Rees-Herdman <tom@techanvil.co.uk>
2020-01-16 06:43:11 +0000

    more tea

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+// 'Hello World' nodejs12.x runtime AWS Lambda function
+exports.handler = (event, context, callback) => {
+  console.log('Hello, logs!!!!!!');
+  callback(null, 'great success');
+}
